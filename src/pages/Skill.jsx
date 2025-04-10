5134c82a27408abfdfb687988e25ae6f85c4bfc9
haran2005 <hariharan.srk2005@gmail.com>
2025-04-10 19:58:24 +0530

    frist push

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -4,7 +4,16 @@ import SkilsLevel from '../components/SkilsLevel'
 function Skill() {
   return (
     <>
-    
+    <section className="w-full h-auto m-auto">
+        <h2 className="heading text-center text-3xl">
+            Proficiency
+        </h2>
+        <div className="w-1/2 lg:w-full h-auto p-5">
+            <SkilsLevel
+            title='Frontend'
+            width={800}/>
+        </div>
+    </section>
     </>
   )
 }
